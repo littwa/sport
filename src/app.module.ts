736e6dbc053bd976ae0df331bf55b655c93553db
littwa <littwa777@gmail.com>
2022-12-05 02:41:49 +0200

    unexpect77

--- src/app.module.ts
+++ src/app.module.ts
@@ -40,7 +40,40 @@ const storage2 = multer.diskStorage({
   imports: [
     PassportModule,
     ConfigModule.forRoot({
-      load: [configuration],
+      load: [() => ({
+        nodemailerTransporterConfig: {
+          transport: {
+            host: 'smtp.gmail.com',
+            secure: false,
+            // port: 465,
+            auth: {
+              user: process.env.NODEMAILER_USER,
+              pass: process.env.NODEMAILER_PASSWORD,
+            },
+          },
+          defaults: {
+            from: '"No Reply" <noreply@example.com>',
+          },
+        },
+        jwtExpires: {
+          _30days: {
+            exp: '30d',
+            expIncrement: 2592000000,
+          },
+          _60Seconds: {
+            exp: '60s',
+            expIncrement: 60000,
+          },
+          _300Seconds: {
+            exp: '300s',
+            expIncrement: 300000,
+          },
+          _1hour: {
+            exp: '1h',
+            expIncrement: 3600000,
+          },
+        },
+      })],
       isGlobal: true,
       envFilePath: ['.env'], // not necessary
     }),
