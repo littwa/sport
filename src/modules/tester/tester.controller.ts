fc233b1f06f037eeb9f5e3099ddd1e2c4cfaaec6
littwa <littwa777@gmail.com>
2024-01-23 18:19:45 +0200

    deplo12

--- src/modules/tester/tester.controller.ts
+++ src/modules/tester/tester.controller.ts
@@ -105,7 +105,7 @@ export class TesterController {
         return 'ok';
     }
 
-    @ApiOperation({ summary: 'Base test endpoint.' })
+    @ApiOperation({ summary: 'Base test endpoint get' })
     @ApiResponse({ status: 200, description: 'Success' })
     @ApiResponse({ status: 404, description: 'Error' })
     @Get('base')
